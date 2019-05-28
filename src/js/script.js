446c9f43586375bc66022b1675fd616545bc6f86
Karolina Nowak <nowak.karolina90@gmail.com>
2019-05-28 21:04:49 +0200

    Add instance for each product

--- src/js/script.js
+++ src/js/script.js
@@ -63,8 +63,9 @@
         initMenu: function() {
             const thisApp = this;
             console.log('thisApp.data: ', thisApp.data);
-            const testProduct = new Product();
-            console.log('testProduct: ', testProduct);
+            fot(let productData in thisApp.data.products) {
+                new Product(productData, thisApp.data.products[productData]);
+            }
         },
         initData: function() {
             const thisApp = this;
